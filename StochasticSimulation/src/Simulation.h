b03150abf2355b0d76b2c7a14bf792dcd4473de2
Ivikhostrup <ihostr16@student.aau.dk>
2023-05-27 12:44:43 +0200

    Update Simulation.h

--- StochasticSimulation/src/Simulation.h
+++ StochasticSimulation/src/Simulation.h
@@ -6,9 +6,29 @@
 #define STOCHASTICSIMULATION_SIMULATION_H
 
 
-class Simulation {
+#include <vector>
+#include "Reaction.h"
 
+class Simulation {
+public:
+
+    Simulation operator()(const CombinedReactants& reactants, const Species& product) {
+        AddCombinedReactants(reactants);
+        AddProduct(product);
+
+        return *this;
+    }
+
+    void AddCombinedReactants(const CombinedReactants& reactants);
+    void AddProduct(const Species& product);
+
+    friend std::ostream& operator<<(std::ostream& os, const Simulation& simulation);
+private:
+    CombinedReactants m_combinedReactants;
+    Species m_species;
 };
+
+
 
 
 #endif //STOCHASTICSIMULATION_SIMULATION_H
